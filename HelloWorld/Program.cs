60b0adb27d54ccfd98a4931bcb8410247296ad87
Nickolas Reid <nickolaswdreid@gmail.com>
2017-06-08 22:01:48 -0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void CallToChildThread()
+        {
+            Console.WriteLine("Child thread starts");
+        }
+
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
